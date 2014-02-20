074a46ecc363b7fe52fa4d6b8ab6a9f5ecf7c9bb
Jan Dvořák <dvorak@ebrana.cz>
2014-02-20 17:03:39 +0100

    Modify runscript

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 forever stop broker.js
-forever start -a -p . -o logs/out.log -e logs/err.log -l logs/forever.log --pidFile forever.pid -w broker.js
+forever start -a -p . -o logs/out.log -e logs/err.log -l logs/forever.log --pidFile forever.pid -w --watchIgnore "logs/*" broker.js
