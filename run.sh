fb01a914721a48d4cb27a18792bec585829a84a5
Jan Dvořák <dvorak@ebrana.cz>
2014-04-21 22:07:21 +0200

    auto-create and gitignore log directory

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+mkdir -p logs
 forever stop broker.js
 forever start -a -p . -o logs/out.log -e logs/err.log -l logs/forever.log --pidFile forever.pid -w --watchIgnore "logs/*" broker.js
