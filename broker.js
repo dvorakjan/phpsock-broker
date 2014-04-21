be2ed3aab8d12fbfa76f3e56c4f377a614d91c1a
Jan Dvořák <dvorak@ebrana.cz>
2014-04-21 22:08:32 +0200

    repair code formatting

--- broker.js
+++ broker.js
@@ -1,8 +1,7 @@
-var wampio  = require('wamp.io'),
-    sockjs  = require('sockjs'),
+var wampio = require('wamp.io'),
+    sockjs = require('sockjs'),
     connect = require('connect'),
-    dnode   = require('dnode');
-
+    dnode = require('dnode');
 
 
 // ----- Base HTTP server + static content -----
@@ -10,7 +9,7 @@ var server = connect()
     .use(connect.static(__dirname + '/public'))
     .listen(9000);
 console.log('Socket server listening on 9000.')
- 
+
 // ----- WAMP server -----
 var wamp = new wampio.Server();
 var sockjsServer = sockjs.createServer({
@@ -19,44 +18,46 @@ var sockjsServer = sockjs.createServer({
 });
 wamp.clientsByAlias = {};
 sockjsServer.installHandlers(server);
-sockjsServer.on('connection', function(client) {
-  client.send = client.write;
-  client.on('data', function(data) {
-    client.emit('message', data);
+sockjsServer.on('connection', function (client) {
+    client.send = client.write;
+    client.on('data', function (data) {
+        client.emit('message', data);
 
-    // TODO ukladat PubSub eventy do monga nebo posilat do PHP
-  });
-  client.on('end', function(a,b,c) {
-    wamp.publish('http://ebrana.cz/chat', client.alias+' disconnect');
-  });
+        // TODO ukladat PubSub eventy do monga nebo posilat do PHP
+    });
+    client.on('end', function () {
+        wamp.publish(client.uri+'#userOffline', client.alias);
+    });
 
-  wamp.onConnection(client);
+    client.rooms = {};
+    wamp.onConnection(client);
 });
 
 // ----- dNode server -----
 var dnodeServer = dnode(function (remote, conn) {
-    this.getOnlineClients = function (callback) { 
-      callback(Object.keys(wamp.clientsByAlias)) 
+    this.getOnlineClients = function (callback) {
+        callback(Object.keys(wamp.clientsByAlias))
     };
 
-    this.callClient       = function (alias, procedure, params, callback) { 
-      if (alias in wamp.clientsByAlias) {
-        wamp.clientsByAlias[alias].call(procedure, params, function(res){
-          callback(res);
-        })
-      } else {
+    this.callClient = function (alias, procedure, params, callback) {
+        if (alias in wamp.clientsByAlias) {
+            for (var i in wamp.clientsByAlias[alias]) {
+                wamp.clientsByAlias[alias][i].call(procedure, params, function (res) {
+                    callback(res);
+                })
+            }
+        } else {
+            callback();
+        }
+    };
+
+    this.publish = function (topic, message, callback) {
+        wamp.publish(topic, message);
         callback();
-      }
-    };
-
-    this.publish          = function (topic, message, callback) { 
-      wamp.publish(topic, message);
-      callback();
     };
 });
 dnodeServer.listen(7070);
 console.log('DNode server listening on 7070.')
-
 
 
 // FUKNCE BROKERU
@@ -70,24 +71,83 @@ console.log('DNode server listening on 7070.')
 // - spravuje seznam mistnosti a klientu v nich, informuje ostatni klienty o pripojeni/odpojeni ostatnich klientu
 
 
+var rooms = {};
+wamp.on('call', function (procUri, args, cb, client) {
+    // wamp.publish('http://ebrana.cz/news', {'a': 'bcauec uvodni zprava vid'})
 
-wamp.on('call', function(procUri, args, cb, client) {
-  wamp.publish('http://ebrana.cz/news', {'a':'bcauec'})
+    switch (procUri) {
 
-  switch (procUri) {
-    
-    case 'registerClientAlias': 
-      wamp.clients[client.id].alias = args[0];
-      wamp.clientsByAlias[args[0]] = client;
-      break;
-    
-    case 'isEven':
-      wamp.clientsByAlias['jan-dvorak'].call('test', {param1:12444}, function(res){
-        console.log(res)
-      })
+        case 'roomCreate': // roomUri
+            var hash = Math.round(Math.random() * 100000);
+            rooms[args[0]] = {
+                uri: args[0],
+                hash: hash,
+                users: [wamp.clients[client.id].alias]
+            }
+            cb(null, hash);
+            break;
 
-      cb(null, args[0] % 2 == 0);
-      break;
-  }
-  
+        case 'roomInvite': // userAlias, roomUri
+            // pokud mistnost existuje a uzivatel, ktery chce pridat dalsiho v ni je
+            if (args[1] in rooms && rooms[args[1]].users.indexOf(wamp.clients[client.id].alias) != -1) {
+                // pokud uzivatel v seznamu uz neni
+                if (rooms[args[1]].users.indexOf(args[0]) == -1) {
+                    rooms[args[1]].users.push(args[0]);
+                }
+            }
+            cb();
+            break;
+
+        case 'roomMessage': // roomUri, message
+            var roomId = args[0];
+            
+            // TODO verejne mistnosti resit jako pubsub
+            if (roomId in rooms && rooms[roomId].users.length <=1) {
+                
+                // for (var i in wamp.clients) {
+                //     var alias = wamp.clients[i].alias;
+                //     // TODO delivery confirmation
+                //     for (var i in wamp.clientsByAlias[alias]) {
+                //         wamp.clientsByAlias[alias][i].call('roomMessage', {room: roomId, message: args[1], author: {name: wamp.clients[client.id].alias}});
+                //     }
+                // }
+            } else if (roomId in rooms && rooms[roomId].users.indexOf(wamp.clients[client.id].alias) != -1) {
+                for (var i in rooms[roomId].users) {
+                    var alias = rooms[roomId].users[i];
+                    if (alias in wamp.clientsByAlias) {
+                        // TODO delivery confirmation
+                        for (var i in wamp.clientsByAlias[alias]) {
+                            wamp.clientsByAlias[alias][i].call('roomMessage', {room: roomId, message: args[1], author: {name: wamp.clients[client.id].alias}});
+                        }
+                    }
+                }
+            } else {
+                console.log('Invalid room or user.', args[0], wamp.clients[client.id].alias, rooms[roomId]);
+            }
+            cb();
+            break;
+
+        case 'registerUsername': // username, uri
+            wamp.clients[client.id].alias = args[0];
+            wamp.clients[client.id].uri = args[1];
+            console.log(args[1]+'#userOnline')
+            wamp.publish(args[1]+'#userOnline', args[0]);
+
+            if (!(args[0] in wamp.clientsByAlias)) {
+                wamp.clientsByAlias[args[0]] = [];
+            }
+            wamp.clientsByAlias[args[0]].push(client);
+            console.log('client '+args[0]+' joined, online clients '+Object.keys(wamp.clientsByAlias).join(','))
+            cb(null, Object.keys(wamp.clientsByAlias));
+            break;
+
+        case 'isEven':
+//            wamp.clientsByAlias['jan-dvorak'].call('test', {param1: 12444}, function (res) {
+//                console.log(res)
+//            })
+
+            cb(null, args[0] % 2 == 0);
+            break;
+    }
+
 });
