16a6d125d078b528dff89b19e7a2e0197ae87ea3
Jan Dvořák <dvorak@ebrana.cz>
2014-06-11 08:42:15 +0200

    Improved CLI output of run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 mkdir -p logs
-forever stop broker.js
-forever start -a -p . -o logs/out.log -e logs/err.log -l logs/forever.log --pidFile forever.pid -w --watchIgnore "logs/*" broker.js
+forever stop broker.js > /dev/null 2>&1
+forever start -a -p . --minUptime 1000 --spinSleepTime 100 -o logs/out.log -e logs/err.log -l logs/forever.log --pidFile forever.pid -w --watchIgnore "logs/*" broker.js
